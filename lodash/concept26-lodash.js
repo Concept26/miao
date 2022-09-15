4061dbfc5d8de9bc2a52478f648decd33da8180b
cco <111@qq.com>
2022-09-15 22:23:46 +0800

    d98

--- lodash/concept26-lodash.js
+++ lodash/concept26-lodash.js
@@ -150,6 +150,141 @@ var concept26 = {
       } 
     }
   return -1
-  
+  },
+
+  //减少一级array嵌套深度
+  flatten: function (arry) {
+    let result = []
+    for (let i = 0; i < arry.length; i++) {
+      if (Array.isArray(arry[i])) {
+        for (let each of arry[i]) {
+          result.push(each)
+        }
+      } else {
+        result.push(arry[i])
+      }
+    }
+    return result
+  },
+
+  // 将array递归为一维数组。
+  flattenDeep: function (arry) {
+    let result = []
+    fDeep(arry)
+    function fDeep(arry) {
+      for (let i = 0; i < arry.length; i++) {
+        if (Array.isArray(arry[i])) {
+          fDeep(arry[i])
+        } else {
+          result.push(arry[i])
+        }
+      }
+    }
+    return result
+  },
+
+  //根据 depth 递归减少 array 的嵌套层级
+  flattenDepth: function (arry, depth = 1) {
+    let result = [], rec = 0 // rec 记录递归次数
+    fDeep(arry, depth)
+    function fDeep(arry, depth) {
+      for (let i = 0; i < arry.length; i++) {
+        if (Array.isArray(arry[i])) {
+          rec++
+          if (rec > depth) {
+            result.push(arry[i])
+            rec = 0   // 递归次数大于目标值时清零
+          } else {
+            fDeep(arry[i], depth)
+          }
+        } else {
+          result.push(arry[i])
+        }
+      }
+    }
+    return result
+  },
+
+  //返回一个由键值对pairs构成的对象
+  fromPairs: function (pairs) {
+    let result = {}
+    for (let i = 0; i < pairs.length; i++) {
+      result[pairs[i][0]] = pairs[i][1]
+    }
+    return result
+  },
+
+  //返回数组 array 的第一个元素
+  head: function (array) {
+    return array[0]
+  },
+
+  //返回首次 value 在数组array中被找到的 索引值， 如果 fromIndex 为负值，将从数组array尾端索引进行匹配
+  indexOf: function (array, value, fromIndex = 0) {
+    if (fromIndex >= 0) {
+      for (let i = fromIndex; i < array.length; i++) {
+        if (array[i] == value) return i
+      }
+    } else {
+      for (let i = array.length + fromIndex; i < array.length; i++) {
+        if (array[i] == value) return i
+      }
+    }
+    return -1
+  },
+
+  // 从右到左遍历array的元素
+  lastIndexOf: function (array, value, fromIndex) {
+    if (!fromIndex && !(fromIndex == 0)) fromIndex = array.length - 1
+    for (let i = fromIndex; i >= 0; i--) {
+      if (array[i] == value) return i
+    }
+    return -1
+  },
+
+  //获取数组array中除了最后一个元素之外的所有元素
+  initial: function (array) {
+    let result = []
+    for (let i = 0; i < array.length - 1; i++) {
+      result.push(array[i])
+    }
+    return result
+  },
+
+  //将 array 中的所有元素转换为由 separator 分隔的字符串
+  join: function (array, separator = ',') {
+    let result = '', has = false // 记录是否第一次循环
+    for (let i = 0; i < array.length; i++) {
+      if (has) result += separator //第一次循环不加分隔符
+      result += array[i] 
+      has = true
+    }
+    return result 
+  },
+
+  //获取array中的最后一个元素
+  last: function (array) {
+    return array[array.length - 1]
+  },
+
+  //移除数组array中所有和给定值相等的元素，这个方法会改变数组
+  pull: function (array, ...values) {
+    for (let i = 0; i < array.length; i++) {
+      if (values.includes(array[i])) {
+        array.splice(i, 1) // 从指定位置删除，修改原数组
+        i--
+      }
+    }
+    return array
+  },
+
+  //反转array，这个方法会改变原数组
+  reverse: function (array) {
+    for (let i = 0, j = array.length - 1; i < j; i++, j--) { // 双指针反转原数组
+      let temp = array[i]
+      array[i] = array[j]
+      array[j] = temp
+    }
+    return array
   }
 }
